0b6d47646cec8ef5fafd70dce163796aa6189749
mirina24 <mirina.zegaoui@edu.ece.fr>
2025-06-04 14:33:48 +0200

    creation du component facesnapcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FaceSnapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'snapface';
-}
+
+export class AppComponent {}
