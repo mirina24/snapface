ded1a36271bf2e67d797299cddd1cbf05ce0f856
mirina24 <mirina.zegaoui@edu.ece.fr>
2025-06-05 21:24:05 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnap } from './models/face-snap';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,36 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+export class AppComponent implements OnInit {
 
-export class AppComponent {}
+  mySnap!: FaceSnap;
+  secondSnap!:FaceSnap;
+
+  faceSnaps!: FaceSnap[];
+
+  ngOnInit() {
+
+    this.faceSnaps = [
+    this.mySnap = new FaceSnap(
+      'Archibald',
+      'Mon meilleur ami depuis tout petit !',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      5
+    ),
+    this.secondSnap = new FaceSnap(
+      'Buffet',
+      'Meilleur nourriture du monde !',
+      'https://gourmandiz.dhnet.be/wp-content/uploads/2018/04/petitdejvienn.jpg',
+      new Date(),
+      2
+    )
+    ];
+
+    this.faceSnaps[1].setLocation('à Paris');
+  }
+}
+
+
+
+
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit{
+  @Input() faceSnap!: FaceSnap;
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
+
+  ngOnInit(): void {
+  this.snapButtonText = 'Oh, Snap!'; 
+  this.userHasSnapped = false;
+  }
+
+  onSnap(): void{
+    if (this.userHasSnapped){
+      this.unSnap();
+    }
+    else{
+      this.snap();
+    }
+  }
+
+
+  unSnap() {
+    this.faceSnap.removeSnap();
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
+  }
+
+  snap() {
+    this.faceSnap.addSnap();
+    this.snapButtonText = 'Oops, unSnap!';
+    this.userHasSnapped = true;
+  }
 
 }
+
+
